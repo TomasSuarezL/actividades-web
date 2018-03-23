350f345fa4c7b9b3e80beee26fcc8a2cbd7dbf45
SuarezLissi <tsuarezlissi@cariverplate.com.ar>
2018-03-23 16:23:37 -0300

    estadisticas v1.0

--- dist/bundle.js
+++ dist/bundle.js
@@ -67,8 +67,9 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-// import $ from 'jquery';
-// import printMe from './print.js';
+var baseActividadesUrl = "http://192.168.2.4/actividadesapi/api/";
+
+
 $(document).ready(function() {
 
     $('.navbar').append('<button type="button" class="btn-back" ><i class="material-icons">exit_to_app</i></button>').click(() => {
@@ -85,16 +86,14 @@ $(document).ready(function() {
         $('.loader').fadeIn(200);
         let actividad = $("#actividad").val();
         let ano = $("#ano").val();
-        console.log(actividad, ano);
 
-        $.ajax({
-            type: "GET",
-            beforeSend: function(request) {
-              request.setRequestHeader("Authorization", `Bearer ${window.sessionStorage.accessToken}`);
-            },
-            url: `http://192.168.2.4/actividadesapi/api/deportistas/${actividad}?year=${ano}`,
-          })
+        var listPromise = getWithAuthorization(`${baseActividadesUrl}deportistas/${actividad}?year=${ano}`);
+
+        var statsPromise = getWithAuthorization(`${baseActividadesUrl}deportistas/cadetes/stats/debitos`);
+
+        Promise.all([listPromise,statsPromise])
         .then(function(data){
+            console.log(data);
             $("#deportistas-table").bind('dynatable:afterUpdate', (x,y) => {
                 let trs = $('tbody tr');
                 for (let i=0; i < trs.length; i++){
@@ -110,21 +109,28 @@ $(document).ready(function() {
     
             let dynatable = $('#deportistas-table').dynatable({
                 dataset: {
-                  records: data
+                  records: data[0]
                 }
             }).data('dynatable');
 
-            dynatable.settings.dataset.originalRecords = data;
+            dynatable.settings.dataset.originalRecords = data[0];
             dynatable.process();
     
+            showList();
             $('.loader').fadeOut(300);
             $(".table-container").fadeIn(300);
 
-            console.log(calcularTotales(data));
+            loadAndRenderStats(data[1]);
 
         }).catch((err)=> { alert(err.statusText)});
-    
+
+        
+
     }).click();
+
+
+    $('#btn-list').click( showList );
+    $('#btn-stats').click( showStats );
 
 });
 
@@ -142,6 +148,44 @@ function calcularTotales(data){
 
     $(".totales").append(`<div>Importe Total: ${total} $</div><div>Total: ${data.length}</div><div>Habilitados: ${habilitados.length}</div>`);
     return data.length;
+}
+
+function showList() {
+    $('#btn-list').addClass('selected');
+    $('#btn-stats').removeClass('selected');
+
+    $('#tab-list').fadeIn(100);
+    $('#tab-stats').fadeOut(100);
+
+}
+
+function showStats() {
+    $('#btn-stats').addClass('selected');
+    $('#btn-list').removeClass('selected');
+
+    $('#tab-stats').fadeIn(100);
+    $('#tab-list').fadeOut(100);
+
+}
+
+function loadAndRenderStats(stats) {
+
+    let card = $('<div class="stats-card"></div>');
+    card.append("<div class='stats-row header'><i class='material-icons'>monetization_on</i><p>Cobros Débitos</p></div>");
+    card.append(stats.map(val => `<div class='stats-row'><p>${moment().month(val.month-1).format("MMMM")}</p><p>${val.descripcion}</p><p>${val.importe} $</p></div>`));
+
+    $('#column-1').append(card);
+}
+
+
+function getWithAuthorization(url) {
+    return $.ajax({
+        type: "GET",
+        beforeSend: function(request) {
+          request.setRequestHeader("Authorization", `Bearer ${window.sessionStorage.accessToken}`);
+        },
+        url: url,
+    })
 }
 
 
