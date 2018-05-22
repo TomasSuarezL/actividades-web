c7540d3627eed1e4e84336ee231960ef8046900a
SuarezLissi <tsuarezlissi@cariverplate.com.ar>
2018-05-22 18:14:22 -0300

    Agrego CORS al servidor

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-var express = require('express'), app = express(),
- bodyParser = require("body-parser");
+var express = require('express'),
+    app = express(),
+    bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 var request = require('request');
-var exphbs  = require('express-handlebars');
-var morgan      = require('morgan');
-var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var exphbs = require('express-handlebars');
+var morgan = require('morgan');
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var session = require('express-session');
 var config = require('./config'); // get our config file
 const PORT = process.env.PORT || 3000;
 // var todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/dist'));
@@ -26,9 +29,19 @@ app.use(session({
     }
 }));
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 app.set('superSecret', config.secret); // secret variable
 
-app.engine('handlebars', exphbs({defaultLayout: 'main', partialsDir: __dirname + '/views/partials/'}));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    partialsDir: __dirname + '/views/partials/'
+}));
 app.set('view engine', 'handlebars');
 
 var sessionChecker = (req, res, next) => {
@@ -37,48 +50,67 @@ var sessionChecker = (req, res, next) => {
         res.redirect('/login');
     } else {
         next();
-    }    
+    }
 };
 
-app.get('/', sessionChecker ,function(req,res){   
-    res.render("main");  
+app.get('/', sessionChecker, function (req, res) {
+    res.render("main");
 });
 
-app.get('/login', function(req,res){
-    if (!req.session.user) res.render("login",{layout: false})
-    else res.redirect('/'); 
- });
+app.get('/login', function (req, res) {
+    if (!req.session.user) res.render("login", {
+        layout: false
+    })
+    else res.redirect('/');
+});
 
- //TODO: usar request-promise
- app.post('/login', function(req,response){
+//TODO: usar request-promise
+app.post('/login', function (req, response) {
     let urlAuth = "http://192.168.2.4/actividadesapi/api/usuarios/authenticate";
 
-    request.post({url: urlAuth, form: {username: req.body.username, password:req.body.password}}, function(err,httpResponse,body){ 
+    request.post({
+        url: urlAuth,
+        form: {
+            username: req.body.username,
+            password: req.body.password
+        }
+    }, function (err, httpResponse, body) {
         if (httpResponse.statusCode == 200) {
             req.session.token = body;
             req.session.user = req.body.username;
-            response.json({"usuario": req.session.user, "lastUrl": req.session.lastUrl,"token": req.session.token});
-        }
-        else {
+            response.json({
+                "usuario": req.session.user,
+                "lastUrl": req.session.lastUrl,
+                "token": req.session.token
+            });
+        } else {
             console.log(httpResponse);
-            response.json({"statusCode": httpResponse.statusCode, "body": body})
+            response.json({
+                "statusCode": httpResponse.statusCode,
+                "body": body
+            })
         }
     });
 
- });
+});
 
- app.get('/logout', function(req,res){
+app.get('/logout', function (req, res) {
     if (req.session.user) {
         req.session.destroy();
     }
-    res.json({status: "logout"}); 
- });
-
- app.get('/deportista/:deportistaId', sessionChecker, function(req,res){
-    var id = req.params.deportistaId;
-    res.render('deportista', {layout: false, id:id});
+    res.json({
+        status: "logout"
+    });
 });
 
- app.listen(PORT , function(){
-    console.log("APP IS RUNNING ON PORT "+ PORT);
+app.get('/deportista/:deportistaId', sessionChecker, function (req, res) {
+    var id = req.params.deportistaId;
+    res.render('deportista', {
+        layout: false,
+        id: id
+    });
+});
+
+app.listen(PORT, function () {
+    console.log("APP IS RUNNING ON PORT " + PORT);
 });
--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,18 @@
 var baseActividadesUrl = "http://192.168.2.4/actividadesapi/api/";
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     $('.navbar').append('<button type="button" class="btn-back" ><i class="material-icons">exit_to_app</i></button>')
     $('.btn-back').click(() => {
         $(".table-container").fadeOut(200);
         $('.loader').fadeIn(200);
         $.get('/logout')
-        .then((data) => window.location.href = '/login')
-        .catch((err) => console.log(err));
+            .then((data) => window.location.href = '/login')
+            .catch((err) => console.log(err));
     });
 
-    $('#cargar').click(function() {
+    $('#cargar').click(function () {
 
         $(".table-container").fadeOut(200);
         $('.loader').fadeIn(200);
@@ -23,59 +23,61 @@ $(document).ready(function() {
 
         var statsPromise = getWithAuthorization(`${baseActividadesUrl}deportistas/cadetes/stats/debitos`);
 
-        Promise.all([listPromise,statsPromise])
-        .then(function(data){
-            console.log(data);
-            $("#deportistas-table").bind('dynatable:afterUpdate', (x,y) => {
-                let trs = $('tbody tr');
-                for (let i=0; i < trs.length; i++){
-                    if (trs[i].children[12].innerHTML == "Deshabilitado"){
-                        $(trs[i]).addClass("disabled");
+        Promise.all([listPromise, statsPromise])
+            .then(function (data) {
+                console.log(data);
+                $("#deportistas-table").bind('dynatable:afterUpdate', (x, y) => {
+                    let trs = $('tbody tr');
+                    for (let i = 0; i < trs.length; i++) {
+                        if (trs[i].children[12].innerHTML == "Deshabilitado") {
+                            $(trs[i]).addClass("disabled");
+                        }
                     }
-                }
 
-                trs.on('click', function() {
-                    window.location.href = '/deportista/'+ $(this)[0].children[0].innerHTML;
+                    trs.on('click', function () {
+                        window.location.href = '/deportista/' + $(this)[0].children[0].innerHTML;
+                    });
                 });
+
+                let dynatable = $('#deportistas-table').dynatable({
+                    dataset: {
+                        records: data[0]
+                    }
+                }).data('dynatable');
+
+                dynatable.settings.dataset.originalRecords = data[0];
+                dynatable.process();
+
+                showList();
+                $('.loader').fadeOut(300);
+                $(".table-container").fadeIn(300);
+
+                loadAndRenderStats(data[1]);
+                calcularTotales(data[0]);
+
+            }).catch((err) => {
+                alert(err.statusText)
             });
-    
-            let dynatable = $('#deportistas-table').dynatable({
-                dataset: {
-                  records: data[0]
-                }
-            }).data('dynatable');
-
-            dynatable.settings.dataset.originalRecords = data[0];
-            dynatable.process();
-    
-            showList();
-            $('.loader').fadeOut(300);
-            $(".table-container").fadeIn(300);
-
-            loadAndRenderStats(data[1]);
-            calcularTotales(data[0]);
-
-        }).catch((err)=> { alert(err.statusText)});
 
     }).click();
 
 
-    $('#btn-list').click( showList );
-    $('#btn-stats').click( showStats );
+    $('#btn-list').click(showList);
+    $('#btn-stats').click(showStats);
 
 });
 
 
-function calcularTotales(data){
+function calcularTotales(data) {
     $(".totales").empty();
 
     let habilitados = data.filter(x => x.habilitado == "Habilitado")
 
-    let total = habilitados.reduce(function(acc,val){
-        return acc += val.importe * val.cantTurnos * (1-val.descuento_float);
-    },0);
+    let total = habilitados.reduce(function (acc, val) {
+        return acc += val.importe * val.cantTurnos * (1 - val.descuento_float);
+    }, 0);
 
-    
+
 
     $(".totales").append(`<div>Importe Total: ${total} $</div><div>Total: ${data.length}</div><div>Habilitados: ${habilitados.length}</div>`);
     return data.length;
@@ -113,10 +115,9 @@ function loadAndRenderStats(stats) {
 function getWithAuthorization(url) {
     return $.ajax({
         type: "GET",
-        beforeSend: function(request) {
-          request.setRequestHeader("Authorization", `Bearer ${window.sessionStorage.accessToken}`);
+        beforeSend: function (request) {
+            request.setRequestHeader("Authorization", `Bearer ${window.sessionStorage.accessToken}`);
         },
         url: url,
     })
 }
-
